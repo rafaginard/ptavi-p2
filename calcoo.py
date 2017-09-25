2cfc16c9432262c8a3c321a26be7efb0fca967b8
Rafael Ginard Aguilera <rginard@gamma05.aulas.gsyc.urjc.es>
2017-09-25 16:57:34 +0200

    SUMA/RESTA

--- calcoo.py
+++ calcoo.py
@@ -3,9 +3,9 @@
 import sys
 
 class Calculadora:
-##    def __init__(self, valor1, valor2):
-##        self.valor1 = valor1
-##        self.valor2 = valor2
+    def __init__(self, valor1, valor2):
+        self.valor1 = valor1
+        self.valor2 = valor2
     def suma(self):
         return (self.valor1 + self.valor2)
     def resta(self):
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     operacion = Calculadora(int(sys.argv[1]), int(sys.argv[3]))
 
     if sys.argv[2] == "suma":
-        result = op1.suma()
+        result = operacion.suma()
     elif sys.argv[2] == "resta":
-        result = op1.resta()
-    print (result)
+        result = operacion.resta()
+        print (result)
