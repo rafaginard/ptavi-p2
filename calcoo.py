33e55b4994ff4226b458b897dd5a4269a1af5336
Rafael Ginard Aguilera <rginard@gamma07.aulas.gsyc.urjc.es>
2017-09-25 12:54:00 +0200

    Me suma

--- calcoo.py
+++ calcoo.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+class Calculadora:
+    def __init__(self, valor1, valor2):
+        self.valor1 = valor1
+        self.valor2 = valor2
+    def suma(self):
+        return (self.valor1 + self.valor2)
 
-print ("Este va a ser la calculadora")
+
+if __name__ == "__main__":
+    op1 = Calculadora("2", "3")
+    result = op1.suma()
+    print (result)
+    print ("Este va a ser la calculadora")
