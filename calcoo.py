7fb17236fa8047f8cfc9ccdaba21c0f49812728a
Rafael Ginard Aguilera <rginard@gamma07.aulas.gsyc.urjc.es>
2017-09-25 13:08:42 +0200

    Suma/Resta con argumentos

--- calcoo.py
+++ calcoo.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+import sys
 
 class Calculadora:
     def __init__(self, valor1, valor2):
@@ -7,10 +8,15 @@ class Calculadora:
         self.valor2 = valor2
     def suma(self):
         return (self.valor1 + self.valor2)
-
+    def resta(self):
+        return (self.valor1 - self.valor2)
 
 if __name__ == "__main__":
-    op1 = Calculadora("2", "3")
-    result = op1.suma()
+    op1 = Calculadora(int(sys.argv[1]), int(sys.argv[3]))
+    
+    if sys.argv[2] == "suma":
+        result = op1.suma()
+    elif sys.argv[2] == "resta":
+        result = op1.resta()
     print (result)
     print ("Este va a ser la calculadora")
