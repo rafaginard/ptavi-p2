fc76f703f08bf67b1ba55c9ce2429828836ac312
rafaginard <rafaginard@gmail.com>
2017-10-01 12:54:26 +0200

    Programa terminado

--- calcplus.py
+++ calcplus.py
@@ -5,16 +5,19 @@ import sys
 import calculadorahija
 import csv
 
+if len(sys.argv) != 2:
+    sys.exit("Try: ./calcplus.py file")
+
 with open(sys.argv[1], newline="") as csvfile:
     operaciones = csv.reader(csvfile)
     result = 0
+#  Busco la operacion en la primera posicion de cada linea del fichero.
     for operacion in operaciones:
         try:
             operador = operacion[0]
-        except IndexError:
-            print("Can't read operation")
-            sys.exit(0)
-
+        except IndexError:    
+            sys.exit("Can't read operation")
+#  Me quedo con la parte de la lista de los operadores.
         operadores = operacion[1:]
 
         try:
@@ -49,5 +52,4 @@ with open(sys.argv[1], newline="") as csvfile:
                 except TypeError:
                     sys.exit
         except ValueError:
-            print("Error reading value")
-            sys.exit
+            sys.exit("Error reading value")
