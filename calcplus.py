f536ed3bb2dd82c1d561e12a4a14007db27514b0
rafaginard <rafaginard@gmail.com>
2017-10-01 12:21:57 +0200

    Comprobar excepciones

--- calcplus.py
+++ calcplus.py
@@ -2,36 +2,52 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import calculadorahija
 import csv
 
 with open(sys.argv[1], newline="") as csvfile:
     operaciones = csv.reader(csvfile)
     result = 0
     for operacion in operaciones:
-        operador = operacion[0]
+        try:
+            operador = operacion[0]
+        except IndexError:
+            print("Can't read operation")
+            sys.exit(0)
+
         operadores = operacion[1:]
 
-        if operador == "suma":
-            result = int(operacion[1])
-            for operandos in operadores[1:]:
-                result += int(operandos)
+        try:
+            if operador == "suma":
+                result = int(operacion[1])
+                for operandos in operadores[1:]:
+                    operar = calculadorahija.CalculadoraHija(result, int(operandos))
+                    result = operar.suma()
 
-            print(result)
-        elif operador == "resta":
-            result = int(operacion[1])
-            for operandos in operadores[1:]:
-                result -= int(operandos)
+                print(result)
+            elif operador == "resta":
+                result = int(operacion[1])
+                for operandos in operadores[1:]:
+                    operar = calculadorahija.CalculadoraHija(result, int(operandos))
+                    result = operar.resta()
 
-            print(result)
-        elif operador == "multiplica":
-            result = int(operacion[1])
-            for operandos in operadores[1:]:
-                result *= int(operandos)
+                print(result)
+            elif operador == "multiplica":
+                result = int(operacion[1])
+                for operandos in operadores[1:]:
+                    operar = calculadorahija.CalculadoraHija(result, int(operandos))
+                    result = operar.multiplicacion()
 
-            print(result)
-        elif operador == "divide":
-            result = int(operacion[1])
-            for operandos in operadores[1:]:
-                result /= int(operandos)
-
-            print(result)
+                print(result)
+            elif operador == "divide":
+                result = int(operacion[1])
+                try:
+                    for operandos in operadores[1:]:
+                        operar = calculadorahija.CalculadoraHija(result, int(operandos))
+                        result = operar.division()
+                    print(result)
+                except TypeError:
+                    sys.exit
+        except ValueError:
+            print("Error reading value")
+            sys.exit
