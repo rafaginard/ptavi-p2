2c6d0ace9dd289128930498d773ed2a6ef6765bd
Rafael Ginard Aguilera <rginard@gamma19.aulas.gsyc.urjc.es>
2017-10-02 09:25:20 +0200

    Programa terminado

--- calcoo.py
+++ calcoo.py
@@ -2,12 +2,15 @@
 # -*- coding: utf-8 -*-
 import sys
 
+
 class Calculadora:
     def __init__(self, valor1, valor2):
         self.valor1 = valor1
         self.valor2 = valor2
+
     def suma(self):
         return (self.valor1 + self.valor2)
+
     def resta(self):
         return (self.valor1 - self.valor2)
 
@@ -18,4 +21,4 @@ if __name__ == "__main__":
         result = operacion.suma()
     elif sys.argv[2] == "resta":
         result = operacion.resta()
-        print (result)
+        print(result)
