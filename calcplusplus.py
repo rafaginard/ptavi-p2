28b909893bfeafa18075cd53742a46345d8f9d18
rafaginard <rafaginard@gmail.com>
2017-10-02 12:52:41 +0200

    Terminado con mejora y corregido

--- calcplusplus.py
+++ calcplusplus.py
@@ -6,35 +6,37 @@ import calcoohija
 import csv
 
 if len(sys.argv) != 2:
-    sys.exit("Try: ./calcplusplus.py fichero")
-
-with open(sys.argv[1], newline="") as csvfile:
-    operaciones = csv.reader(csvfile)
-    result = 0
+    sys.exit("Try: ./calcplusplus.py file")
+try:
+    with open(sys.argv[1], newline="") as csvfile:
+        operaciones = csv.reader(csvfile)
+        result = 0
 #  Busco la operacion en la primera posicion de cada linea del fichero.
-    for operacion in operaciones:
-        try:
-            operador = operacion[0]
-        except IndexError:
-            sys.exit("Can't read operation")
-#  Me quedo con la parte de la lista de los operadores.
-        operadores = operacion[1:]
-        result = int(operacion[1])
-        for operandos in operadores[1:]:
+        for operacion in operaciones:
             try:
-                operar = calcoohija.CalculadoraHija(result, int(operandos))
-            except ValueError:
-                sys.exit("Error reading value")
-
-            if operador == "suma":
-                result = operar.suma()
-            elif operador == "resta":
-                result = operar.resta()
-            elif operador == "multiplica":
-                result = operar.multiplicacion()
-            elif operador == "divide":
+                operador = operacion[0]
+            except IndexError:
+                sys.exit("Can't read operation")
+#  Me quedo con la parte de la lista de los operadores.
+            operadores = operacion[1:]
+            result = int(operacion[1])
+            for operandos in operadores[1:]:
                 try:
-                    result = operar.division()
-                except TypeError:
-                    sys.exit
-        print(result)
+                    operar = calcoohija.CalculadoraHija(result, int(operandos))
+                except ValueError:
+                    sys.exit("Error reading value")
+
+                if operador == "suma":
+                    result = operar.suma()
+                elif operador == "resta":
+                    result = operar.resta()
+                elif operador == "multiplica":
+                    result = operar.multiplicacion()
+                elif operador == "divide":
+                    try:
+                        result = operar.division()
+                    except TypeError:
+                        sys.exit
+            print(result)
+except FileNotFoundError:
+    sys.exit("File not found")
